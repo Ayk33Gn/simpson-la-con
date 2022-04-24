fa4cc558451539a38067bdad473d917bb72c11ff
AYKUT GUNDOGDU <aykutgundogdu33@gmail.com>
2022-04-24 11:35:04 +0200

    JenAiMarre

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+import './App.css'
+import Quotes from './components/simpsonQuotes';
+function App() {
+  return (
+    <div className="App">
+    <Quotes />
+    </div>
+  )
+}
+export default App
